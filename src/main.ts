9ff9282ce3dded2bbf801bc1ebc476da512b3b5e
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-04-28 17:29:06 -0400

    feat: transform http rest into microservice

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { envs } from './config';
 
 const bootstrap = async () => {
   const logger = new Logger('Bootstrap');
-
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,8 +23,9 @@ const bootstrap = async () => {
     }),
   );
 
-  await app.listen(envs.port);
-  logger.log(`Server is running on port ${envs.port}`);
+  await app.listen();
+
+  logger.log(`Products Microservice is running on port ${envs.port}`);
 };
 
 bootstrap();
