3b0852ce0952ce5a8f0bab87a602c45cf9e8f298
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-04-28 17:30:09 -0400

    fix: remove unused http verbs from products controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,14 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
-  Query,
-} from '@nestjs/common';
+import { Body, Controller, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -18,22 +8,18 @@ import { ProductsService } from './products.service';
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
-  @Get()
   findAll(@Query() pagination: PaginationDto) {
     return this.productsService.findAll(pagination);
   }
 
-  @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: string) {
     return this.productsService.findOne(+id);
   }
 
-  @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: string,
     @Body() updateProductDto: UpdateProductDto,
@@ -41,7 +27,6 @@ export class ProductsController {
     return this.productsService.update(+id, updateProductDto);
   }
 
-  @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: string) {
     return this.productsService.remove(+id);
   }
